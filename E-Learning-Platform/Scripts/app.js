1dfbe3d1909ec2dd2e6701a2e9d6fcbbf1ecd11e
Abhisek271 <abhisekjena38@gmail.com>
2025-10-25 10:33:36 +0530

    changes dropdown profile

---
+++ E-Learning-Platform/Scripts/app.js
@@ -0,0 +1,18 @@
+﻿
+
+    function toggleDropdown() {
+                var dropdown = document.getElementById("myDropdown");
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+            }
+
+
+    window.onclick = function (event) {
+                if (!event.target.matches('.dropdown button')) {
+                    var dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+                    }
+                }
+            }
+
+
